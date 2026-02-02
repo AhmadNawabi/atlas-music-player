b7cff3513d062ababc59d879c87592c17d88f470
AhmadNawabi <ahmadshamoon0505@gmail.com>
2026-02-01 22:11:11 -0600

    update files

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: "jsdom",
-        setupFiles: './src/setupTests.ts'
+        setupFiles: './src/setupTests.ts',
+        globals: true,
+        coverage: {
+            reporter: ['html']
+        },
     },
 });
